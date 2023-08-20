0bfc03397c6bcb72d21c5e65681252351279979c
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-21 00:42:00 +0600

    recheck

--- index.js
+++ index.js
@@ -1,61 +1,57 @@
 
-let allTotalPrice = 0;
+let totalPriceCount = 0;
+
+const allTotalPrice = document.getElementById('all-total-price');
+const totalPrice = document.getElementById('total-price');
+const discount = document.getElementById('discount');
+const applyButton = document.getElementById('apply-button');
+const makePurchaseButton = document.getElementById('purchase');
 
 function productCurd(card) {
     // card title
     const productTitle = card.childNodes[3].childNodes[3].innerText;
     // cards title
-    const addProductTitle = card.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[1];
-    // cards title count
-    const count = addProductTitle.childElementCount;
+    const productTitleInContainer = card.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[1];
+    // cards title countTitleInContainer
+    const countTitleInContainer = productTitleInContainer.childElementCount;
     // created element for cards title
     const p = document.createElement('p');
-    p.innerHTML = ` ${count + 1}.  ${productTitle} `;
+    p.innerHTML = ` ${countTitleInContainer + 1}.  ${productTitle} `;
     // appendChild of cards title
-    addProductTitle.appendChild(p)
+    productTitleInContainer.appendChild(p)
     // product price
     const productPrice = card.childNodes[3].childNodes[5].childNodes[0].innerText;
     const productPriceNumber = parseFloat(productPrice)
-    // cards total price
-    const totalPrice = card.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[5].childNodes[1].childNodes[0];
-    allTotalPrice = allTotalPrice + productPriceNumber;
-    totalPrice.innerText = allTotalPrice.toFixed(2);
-    // total price after discount
-    const payableTotalAmount = card.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[9].childNodes[1];
-    // make purchase button
-    const makePurchase = card.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[13].childNodes[1];
-    if (productPriceNumber > 0) {
-        makePurchase.style.display = "block";
-    };
-    // coupon apply button
-    const apply = card.parentNode.parentNode.parentNode.childNodes[5].childNodes[1].childNodes[3].childNodes[3];
-    // disable apply button block
-    buttonVisible(allTotalPrice, apply, payableTotalAmount);
+    // const productPriceNumber =  
+    totalPriceCount = totalPriceCount + productPriceNumber;
+    // make purchase button visible
+    if (totalPriceCount > 0) {
+        makePurchaseButton.disabled = false;
+        // add.innerText = totalPriceCount.toFixed(2);
+    }
+    allTotalPrice.innerText = totalPriceCount.toFixed(2);
+    totalPrice.innerText = totalPriceCount.toFixed(2);
+    element(totalPriceCount);
 }
 
-    // apply button block
-function buttonVisible(price, button, add) {
-    if (price >= 200) {
-        button.style.display = "block";
-        add.innerText= allTotalPrice.toFixed(2);
-    }else{
-        add.innerText= allTotalPrice.toFixed(2);
-    };
-};
+function element(totalAmount) {
+    if (totalAmount >= 200) {
+        applyButton.disabled = false;
+        console.log(totalAmount, 'true');
+    } 
+}
 
-document.getElementById('apply-button').addEventListener('click', function(){
+document.getElementById('apply-button').addEventListener('click', function () {
     const coupon = document.getElementById('coupon');
     const couponText = coupon.value;
-    const totalPrice = document.getElementById('total-price').childNodes[1];
-    const discount = document.getElementById('discount');
-    if(couponText === 'SELL200'){
-        discount.innerText= (allTotalPrice*0.2).toFixed(2) ;
-        totalPrice.innerText = (allTotalPrice- (allTotalPrice*0.2)).toFixed(2) ;
+    if (couponText === 'SELL200') {
+        discount.innerText = (totalPriceCount * 0.2).toFixed(2);
+        totalPrice.innerText = (totalPriceCount - (totalPriceCount * 0.2)).toFixed(2);
         console.log('hello')
     }
     coupon.value = '';
 });
 
-document.getElementById('go-home').addEventListener('click', function(){
-    window.location.href= "http://127.0.0.1:5500/index.html";
+document.getElementById('go-home').addEventListener('click', function () {
+    window.location.href = "http://127.0.0.1:5500/index.html";
 })
